44d0f444aa9d5cd8fa06d31318f049887019813e
shwerasingh012 <shweta.singh.ext1@holcim.com>
2022-05-05 12:39:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rlczgh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rlczgh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  counter=0
+  getcounter(type : string){
+type=='add'?this.counter++:this.counter--;
+  }
+}
